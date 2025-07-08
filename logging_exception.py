55c3022fcaf49170ed4731efca8bbaa50eae5441
Yanbang Jia <yanbangjia@gmail.com>
2025-07-08 10:58:31 +0800

    how to print exception logging

---
+++ logging_exception.py
@@ -0,0 +1,29 @@
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
+# 抛出错误
+
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
